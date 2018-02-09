f0faa6d69eb2b7b49e25443e88f52c7655483387
ratsjosh <ratsjosh@outlook.com>
2018-02-09 14:55:45 +0800

    Move loading spinner to Home component

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,27 +1,18 @@
 <template>
   <div>
-    <transition name="fade">
-      <section id="autocomplete-container"
-               v-if="!isFetching">
-        <p class="control has-icons-left">
-          <input class="input"
-                 type="text"
-                 placeholder="Search Routes"
-                 @input="filterRouteSearch($event.target.value)">
-          <span class="icon is-small is-left">
-            <i class="fas fa-search"></i>
-          </span>
-        </p>
-        <panel :input="name"
-               :routes="data.length === 0 ? this.routes.slice(0, 20) : data.slice(0, 5)"></panel>
-      </section>
-    </transition>
-    <div class="spinner"
-         v-if="isFetching">
-      <div class="bounce1"></div>
-      <div class="bounce2"></div>
-      <div class="bounce3"></div>
-    </div>
+    <section id="autocomplete-container">
+      <p class="control has-icons-left">
+        <input class="input"
+               type="text"
+               placeholder="Search Routes"
+               @input="filterRouteSearch($event.target.value)">
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+      <panel :input="name"
+             :routes="data.length === 0 ? this.routes.slice(0, 20) : data.slice(0, 5)"></panel>
+    </section>
   </div>
 </template>
 <script>
@@ -38,8 +29,13 @@ export default {
       data: [],
       name: '',
       selected: null,
-      isFetching: false,
     };
+  },
+  props: {
+    isFetching: {
+      type: Boolean,
+      required: true,
+    },
   },
   components: {
     Panel,
@@ -57,7 +53,7 @@ export default {
         option.to.toLowerCase().indexOf(input.toLowerCase()) >= 0));
     },
     async getRoutesdata() {
-      this.isFetching = true;
+      this.$emit('update:isFetching', true);
       const date = Date.now();
       const limitTrips = 5;
       try {
@@ -71,15 +67,14 @@ export default {
             if (typeof (error) !== 'undefined') {
               throw error;
             }
-            this.isFetching = false;
+            this.$emit('update:isFetching', false);
           } else {
             // Routes found in browser Database
             this.routes = val;
-            this.isFetching = false;
+            this.$emit('update:isFetching', false);
           }
         });
       } catch (error) {
-        console.error(error);
         throw error;
       }
     },
@@ -104,64 +99,6 @@ export default {
 }
 .field:not(:last-child) {
   margin-bottom: 0 !important;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.6s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-.spinner {
-  margin: 100px auto;
-  width: 100%;
-  text-align: center;
-}
-
-.spinner > div {
-  width: 18px;
-  height: 18px;
-  background-color: rgb(153, 153, 153);
-
-  border-radius: 100%;
-  display: inline-block;
-  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-}
-
-.spinner .bounce1 {
-  -webkit-animation-delay: -0.32s;
-  animation-delay: -0.32s;
-}
-
-.spinner .bounce2 {
-  -webkit-animation-delay: -0.16s;
-  animation-delay: -0.16s;
-}
-
-@-webkit-keyframes sk-bouncedelay {
-  0%,
-  80%,
-  100% {
-    -webkit-transform: scale(0);
-  }
-  40% {
-    -webkit-transform: scale(1);
-  }
-}
-
-@keyframes sk-bouncedelay {
-  0%,
-  80%,
-  100% {
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  }
-  40% {
-    -webkit-transform: scale(1);
-    transform: scale(1);
-  }
 }
 </style>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,17 @@
     <navbar></navbar>
     <div class="columns is-gapless">
       <div class="column router-container">
-        <router-view></router-view>
+        <transition name="fade">
+          <div v-if="isFetching"
+               class="spinner-container">
+            <div class="spinner">
+              <div class="bounce1"></div>
+              <div class="bounce2"></div>
+              <div class="bounce3"></div>
+            </div>
+          </div>
+        </transition>
+        <router-view :isFetching.sync="isFetching"></router-view>
       </div>
       <div class="column maps-container is-two-thirds">
         <google-maps></google-maps>
@@ -21,6 +31,7 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js PWA',
+      isFetching: false,
     };
   },
   components: {
@@ -58,6 +69,66 @@ a {
 .router-container {
   height: calc(100vh - 52px);
   margin-top: 52px !important;
+}
+
+.spinner-container {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+
+.spinner {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+}
+
+.spinner > div {
+  width: 18px;
+  height: 18px;
+  background-color: rgb(153, 153, 153);
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+.spinner .bounce1 {
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+
+@-webkit-keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+  }
+}
+
+@keyframes sk-bouncedelay {
+  0%,
+  80%,
+  100% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  40% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
 }
 
 @media (min-width: 320px) and (max-width: 768px) {
